632bdbd16ec9821c4ffa376646f8654ca4449ab9
チセ <123655015+chise0713@users.noreply.github.com>
2024-01-15 12:01:20 +0800

    Initial commit

---
+++ .github/test.sh
@@ -0,0 +1,38 @@
+echo $PATH
+DIRECTORY="$PROTOCAL/$METHOD/$TRANSPORT/$TLS/"
+cat <<EOF
+::group::sing-box version
+$(sing-box version)
+::endgroup::
+EOF
+sing-box run -c $DIRECTORY/client.json >sing-box_client.log 2>&1 &
+SING_CLIENT_PID=$!
+if [[ "$PROTOCAL" == "vmess" ]];then
+    DIRECTORY="$PROTOCAL/$TRANSPORT/$TLS/"
+fi
+if [[ "$PROTOCAL" != "direct" ]];then
+    sing-box run -c $DIRECTORY/server.json >sing-box_server.log 2>&1 &
+    SING_SERVER_PID=$!
+fi
+sleep 1
+iperf3 -s 127.0.0.1 -p 5202 > iperf3_server.log 2>&1 &
+SERVER_PID=$!
+iperf3 -c 127.0.0.1 -p 5201 > iperf3_client.log 2>&1 &
+CLIENT_PID=$!
+wait $CLIENT_PID
+kill $SERVER_PID $SING_SERVER_PID $SING_CLIENT_PID
+
+cat <<EOF
+::group::sing-box server log
+$(cat sing-box_server.log)
+::endgroup::
+::group::sing-box client log
+$(cat sing-box_client.log)
+::endgroup::
+::group::iperf3 server log
+$(cat iperf3_server.log)
+::endgroup::
+::group::iperf3 client log
+$(cat iperf3_client.log)
+::endgroup::
+EOF
---
+++ README.md
@@ -0,0 +1,6 @@
+# speedtest
+As the title said, this is a "standard" protocal speedtest tool.<br>
+results:[![Test protocals' speed](https://github.com/chise0713/speedtest/actions/workflows/speedtest.yml/badge.svg)](https://github.com/chise0713/speedtest/actions/workflows/speedtest.yml)<br>
+<br>
+Or fork this repository and run my workflow by yourself.<br>
+[![Fork me on GitHub](https://img.shields.io/github/forks/chise0713/speedtest.svg?style=social)](https://github.com/chise0713/speedtest/fork)
